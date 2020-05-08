d58dbe8a4dacfe0e17d4e162589ab6f3aed715f6
Ardak <ardasha.aa@gmail.com>
2020-05-08 23:23:09 +0200

    Create tester.py

---
+++ tester.py
@@ -0,0 +1,10 @@
+# tester.py
+
+from linear_optimisation.py import readFile, solveProblem
+
+
+def readFile_test():
+	assert readFile() == [[9,6],[2,1,40],[1,2,50]]
+
+def solveProblem_test():
+	assert solveProblem([[9,6],[2,1,40],[1,2,50]]) == [10.0,20.0]
