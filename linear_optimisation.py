ef01ec1034e2a7d6009b462160e8748a97edf503
Ardak <ardasha.aa@gmail.com>
2020-05-09 12:35:41 +0200

    adapt stickler corrections again

--- linear_optimisation.py
+++ linear_optimisation.py
@@ -19,14 +19,14 @@ def solveProblem(c):
     x2 = p.LpVariable("x2", 0, None, p.LpInteger)
     # objective function
     problem += c[0][0]*x1 + c[0][1]*x2, "Maximise" 
-    for i in range(1,len(c)):
+    for i in range(1, len(c)):
         # constraints are entered
         problem += c[i][0]*x1 + c[i][1]*x2 <= c[i][2], "Contstraint {}".format(i)
     problem.solve()
     return problem
 
 
-def writeFile(c,problem):
+def writeFile(c, problem):
     output = []
     with open("output.txt", "w") as file:
         file.write("Status:" + p.LpStatus[problem.status] + "\n")
@@ -41,6 +41,6 @@ def writeFile(c,problem):
 
 x = readFile()
 y = solveProblem(x)
-z = writeFile(x,y)
+z = writeFile(x, y)
 print(y)
 print("Solution found. Please refer to output.txt")
