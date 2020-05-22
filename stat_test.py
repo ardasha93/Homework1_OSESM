88a3e292d7ea1b587cb944d93abdace8c76962b6
Daniel Huppmann <dh@dergelbesalon.at>
2020-05-22 13:47:53 +0200

    Fix test-function names so that they are exected by pytest

--- stat_test.py
+++ stat_test.py
@@ -6,17 +6,17 @@ from stat_help import readNumbers, mean, variance, stDev
 testList = [2.0, 5.0, 7.0, 4.0, 6.0, 8.0, 9.0, 13.0, 25.0, 1.0]
 
 
-def readNumbers_test():
-    assert readNumbers() == testList
+def test_read_numbers():
+    assert (readNumbers() == testList).all()
 
 
-def mean_test():
+def test_mean():
     assert mean(testList) == 9.0
 
 
-def variance_test():
+def test_variance():
     assert variance(testList) == 43.0
 
 
-def stDev_test():
+def test_stDev():
     assert stDev(testList) == 6.56
